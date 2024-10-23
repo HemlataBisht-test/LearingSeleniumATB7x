a685fab5ba11e36f7a0185e59f809f5be05d6845
HemlataBisht-test <hemlatabisht89@gmail.com>
2024-10-23 21:36:43 +0530

    19 oct

--- src/test/java/com/project/ex_19102024/Selenium014.java
+++ src/test/java/com/project/ex_19102024/Selenium014.java
@@ -1,5 +1,13 @@
 package com.project.ex_19102024;
 
-public class Selenium014 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class Selenium014 {
+@Test
+    public void test(){
+    WebDriver driver=new ChromeDriver();
+    driver.manage().timeouts();
+}
 }
